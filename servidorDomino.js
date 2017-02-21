742f2b1a6ab24cd50cbd2dd723ceeb7b6bf36d0b
MartaGD <marta.483@hotmail.com>
2017-02-21 19:14:57 +0100

    - index HTML - cirdes AJAX 1.0

--- servidorDomino.js
+++ servidorDomino.js
@@ -81,7 +81,7 @@ function iniciar() {
                 "Content-Type": "application/json charset=utf-8"
             });
             var players = numPlayers.length;
-            playedPieces.push(consulta['piece']);
+            //playedPieces.push(consulta['piece']);
             if( players == 0){
                 id = 1;
                 numPlayers.push(1);
@@ -94,6 +94,18 @@ function iniciar() {
             console.log("El jugador "+id+" ha entrat. Num players "+numPlayers.length);
 
             var objecteInicial = {
+                "id" : id,
+                "jugadors": numPlayers
+            }
+
+            response.write(JSON.stringify(objecteInicial));
+            response.end();
+        } else if(pathname == '/start') {
+            response.writeHead(200, {
+                "Content-Type": "application/json charset=utf-8"
+            });
+
+            var objecteJoc = {
                 "id" : id,
                 "pieces":[
                     "0:0","0:1","0:2","0:3","0:4","0:5","0:6",
@@ -108,14 +120,28 @@ function iniciar() {
 
             response.write(JSON.stringify(objecteInicial));
             response.end();
-        } else if(pathname == '/start') {
-
         } else if(pathname == '/playedPiece') {
-            response.writeHead(201, {
+            response.writeHead(200, {
                 "Content-Type": "text/xml; charset=utf-8"
             });
             playedPieces.push(consulta['piece']);
+
+            var objecteJoc = {
+                "id" : id,
+                "pieces":[
+                    "0:0","0:1","0:2","0:3","0:4","0:5","0:6",
+                    "1:0","1:1","1:2","1:3","1:4","1:5","1:6",
+                    "2:0","2:1","2:2","2:3","2:4","2:5","2:6",
+                    "3:0","3:1","3:2","3:3","3:4","3:5","3:6",
+                    "4:0","4:1","4:2","4:3","4:4","4:5","4:6",
+                    "5:0","5:1","5:2","5:3","5:4","5:5","5:6",
+                    "6:0","6:1","6:2","6:3","6:4","6:5","6:6"
+                ]
+            };
+
             console.log("El jugador "+consulta['idJugador']+" ha tirat "+consulta['piece']);
+            response.write(JSON.stringify(objecteJoc));
+            response.end();
 
         } else {
             response.writeHead(404, {
